d56310dfe73b14707cea9f4660d50dfbd397054e
Cristian Cardellino <crscardellino@gmail.com>
2021-05-20 15:02:01 -0300

    lab skeleton

---
+++ src/main/scala/edu/famaf/paradigmas/actors/Supervisor.scala
@@ -0,0 +1,36 @@
+package edu.famaf.paradigmas
+
+import akka.actor.typed.ActorRef
+import akka.actor.typed.Behavior
+import akka.actor.typed.PostStop
+import akka.actor.typed.Signal
+import akka.actor.typed.scaladsl.ActorContext
+import akka.actor.typed.scaladsl.AbstractBehavior
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.LoggerOps
+
+object Supervisor {
+  def apply(): Behavior[SupervisorCommand] = Behaviors.setup(context => new Supervisor(context))
+
+  sealed trait SupervisorCommand
+  final case class Stop() extends SupervisorCommand
+}
+
+class Supervisor(context: ActorContext[Supervisor.SupervisorCommand])
+    extends AbstractBehavior[Supervisor.SupervisorCommand](context) {
+  context.log.info("Supervisor Started")
+
+  import Supervisor._
+
+  override def onMessage(msg: SupervisorCommand): Behavior[SupervisorCommand] = {
+    msg match {
+      case Stop() => Behaviors.stopped
+    }
+  }
+
+  override def onSignal: PartialFunction[Signal, Behavior[SupervisorCommand]] = {
+    case PostStop =>
+      context.log.info("Supervisor Stopped")
+      this
+  }
+}
